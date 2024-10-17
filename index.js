b7dca280e5cbeed702c8d100f65c6db5a3db087c
nikatopu <nikatopu@gmail.com>
2024-10-17 22:31:58 +0400

    Added characters and fetch
      This is a big chunk of an update, since I keep forgetting to commit to main... Anyways, here are some changes:
    
    - Added character layout in HTML;
    
    - Added CSS for pretty much everything that has to do with characters, like listing them properly, animating on hover and the responsiveness;
    
    - Added the fetch function which just updates the current character list (very easy, very demure, very mindful)
    
    - Also moved the closed class into the style.css so I can reuse it ;)

--- index.js
+++ index.js
@@ -5,3 +5,38 @@ dropdownButton.addEventListener('click', () => {
     dropdownItems.classList.toggle('closed');
     dropdownButton.classList.toggle('toggled');
 })
+
+// Getting the API data
+const defaultAPILink = "https://swapi.dev/api/people";
+
+async function getData(link=defaultAPILink) {
+    const result = await fetch(link);
+    console.log(await result.json())
+}
+
+async function updateCharactersByLink(link=defaultAPILink) {
+    // Empty the character list
+    const characterList = document.getElementById('character-list');
+    characterList.innerHTML = "";
+
+    // Fetch the API data
+    const result = await fetch(link);
+    const data = await result.json();
+
+    // For each resulted character, create a new div and append it
+    data.results.forEach(element => {
+        // Create the character div
+        var character = document.createElement('div');
+        character.className = 'character';
+
+        // Create h1 for the character and append it
+        var characterName = document.createElement('h1');
+        characterName.innerHTML = element.name;
+        character.append(characterName);
+
+        // Append the entire character div to the character list
+        characterList.append(character);
+    });
+}
+
+updateCharactersByLink();
