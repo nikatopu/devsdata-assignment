6e0dc4caf166ae070d1b4bdd18643a647df4eff4
nikatopu <nikatopu@gmail.com>
2024-10-18 14:48:12 +0400

    Pagination functionality
    Added the functionality of the pagination buttons & all.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ dropdownButton.addEventListener('click', () => {
 })
 
 // Getting the API data
-const defaultAPILink = "https://swapi.dev/api/people";
+const defaultAPILink = "https://swapi.dev/api/people/";
 
 async function getData(link=defaultAPILink) {
     const result = await fetch(link);
@@ -37,6 +37,49 @@ async function updateCharactersByLink(link=defaultAPILink) {
         // Append the entire character div to the character list
         characterList.append(character);
     });
+
+    // Create a corresponding pagination for the current page
+    const currentPage = data.previous === null ? 1 : parseInt(data.previous.split("=")[1]) + 1;
+    const pagination = document.getElementById('pagination');
+    pagination.innerHTML = "";
+
+    // Add the previous button
+    var previousPage = document.createElement('div');
+
+    previousPage.innerHTML = "<p>previous</p>";
+
+    previousPage.classList.add("page-element");
+    if (data.previous === null) {previousPage.classList.add("page-disabled")}
+
+    previousPage.onclick = () => (updateCharactersByLink(data.previous));
+
+    pagination.append(previousPage);
+
+    // Add buttons for pagination
+    for(var i = 1; i <= 9; i++) {
+        var pageElement = document.createElement("div");
+
+        pageElement.innerHTML = "<p>" + i + "</p>";
+
+        pageElement.classList.add("page-element");
+        if (currentPage === i) {pageElement.classList.add("page-current")}
+
+        pageElement.onclick = (ev) => updateCharactersByLink(defaultAPILink + "?page=" + ev.target.innerHTML.slice(3,4))
+
+        pagination.append(pageElement);
+    }
+
+    // Add the next button
+    var nextPage = document.createElement('div');
+
+    nextPage.innerHTML = "<p>next</p>";
+
+    nextPage.classList.add("page-element");
+    if (data.next === null) {nextPage.classList.add("page-disabled")}
+
+    nextPage.onclick = () => (updateCharactersByLink(data.next))
+
+    pagination.append(nextPage);
 }
 
 updateCharactersByLink();
