08158e0c682e27672c4a03c8a559c70a5371164d
nikatopu <nikatopu@gmail.com>
2024-10-19 11:53:25 +0400

    Enlarging characters
    Added functionality to enlarging character, with neat looking CSS.
    
    When a character is clicked it is transported to the center of the screen with the background being blurred.

--- index.js
+++ index.js
@@ -26,6 +26,9 @@ async function updateCharactersByLink(link=defaultAPILink) {
 
     // For each resulted character, create a new div and append it
     data.results.forEach(element => {
+        // Create a character wrapper for the background blur
+        var characterWrapper = document.createElement('div');
+
         // Create the character div
         var character = document.createElement('div');
         character.className = 'character';
@@ -35,8 +38,30 @@ async function updateCharactersByLink(link=defaultAPILink) {
         characterName.innerHTML = element.name;
         character.append(characterName);
 
+        // Onclick on this character, enlarge it if no other object is enlarged
+        character.onclick = (ev) => {
+            if (document.getElementsByClassName('character-enlarged').length === 0) {
+                // Enlarge the div and the wrapper
+                ev.currentTarget.classList.add('character-enlarged')
+                ev.currentTarget.parentNode.classList.add('character-blur-effect')
+            }
+        };
+
+        // Create a close button
+        var closeButton = document.createElement('div');
+        closeButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="1.5lvw" viewBox="0 0 30 30"><path d="M 7 4 C 6.744125 4 6.4879687 4.0974687 6.2929688 4.2929688 L 4.2929688 6.2929688 C 3.9019687 6.6839688 3.9019687 7.3170313 4.2929688 7.7070312 L 11.585938 15 L 4.2929688 22.292969 C 3.9019687 22.683969 3.9019687 23.317031 4.2929688 23.707031 L 6.2929688 25.707031 C 6.6839688 26.098031 7.3170313 26.098031 7.7070312 25.707031 L 15 18.414062 L 22.292969 25.707031 C 22.682969 26.098031 23.317031 26.098031 23.707031 25.707031 L 25.707031 23.707031 C 26.098031 23.316031 26.098031 22.682969 25.707031 22.292969 L 18.414062 15 L 25.707031 7.7070312 C 26.098031 7.3170312 26.098031 6.6829688 25.707031 6.2929688 L 23.707031 4.2929688 C 23.316031 3.9019687 22.682969 3.9019687 22.292969 4.2929688 L 15 11.585938 L 7.7070312 4.2929688 C 7.5115312 4.0974687 7.255875 4 7 4 z"></path></svg>`;
+        closeButton.className = "character-enlarge-close";
+        closeButton.onclick = (ev) => {
+            ev.stopPropagation(); // Prevents the click event from bubbling up to the character div
+            // Remove the enlarged effects on the character and the character wrapper
+            ev.currentTarget.parentNode.classList.remove('character-enlarged');
+            ev.currentTarget.parentNode.parentNode.classList.remove('character-blur-effect')
+        };
+        character.append(closeButton);
+
         // Append the entire character div to the character list
-        characterList.append(character);
+        characterWrapper.append(character);
+        characterList.append(characterWrapper);
     });
 
     // Create a corresponding pagination for the current page
